927f70dff8eaa1654760cf69e410829ac791813e
lily <hotaru@thinkindifferent.net>
2014-09-07 18:00:25 -0400

    update copyright

--- cal.c
+++ cal.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2008-2010 robert wilson
+/* Copyright (c) 2008-2014 lily wilson
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
